9b96c31604cb1314987a9639c71128c6d9ad7bd5
uarloque <uarloque@live.com>
2022-07-22 06:00:45 +0100

    Update copyContract.js

--- copyContract.js
+++ copyContract.js
@@ -1,13 +1,9 @@
-//<input type="text" value="Hello World" id="myInput">
-//<button onclick = "copyContract()">Copy text</button>
-// image style="cursor: pointer;"
-
+// <button onclick = "copyContract()">Text</button>
 
 function copyContract() {
-  /* Get the text field */
-//  var copyText = "0x0049d29Bf24AEda8dC8a23cc1fe4176B92f16F7e";
+  var copyText = "0xc58A1559b566863668A8C7316da00faC01202300";
   /* Copy the text inside the text field */
-  navigator.clipboard.writeText("0x0049d29Bf24AEda8dC8a23cc1fe4176B92f16F7e");
+  navigator.clipboard.writeText(copyText);
   /* Alert the copied text */
-  alert("$TCU Contract Address Copied! 0x0049d29Bf24AEda8dC8a23cc1fe4176B92f16F7e");
+  alert("$PLT Plata Contract Hash Address Copied! " copyText);
 }
