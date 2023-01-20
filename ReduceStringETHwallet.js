bdd4bf533d3f47b9714a66b415f7f6e02ca02b5d
Adam Soares <uarloque@live.com>
2023-01-20 17:18:33 +0000

    Update ReduceStringETHwallet.js

--- ReduceStringETHwallet.js
+++ ReduceStringETHwallet.js
@@ -3,17 +3,6 @@
     }
 
     function ReducedStringNameWalletAddress() {
-        let WalletAddress = ConnectedWallet();
-        return ( WalletAddress.slice(0,5) + "..." + WalletAddress.slice(-5,-1) );;
-    }
-
-    const connectMetamaskPC = async() =>
-    {    
-        accounts = await window.ethereum.request ( {method : 'eth_requestAccounts'}).catch((err) => {} )
-        changeNetworkToPolygon();
-        
-        console.log( "ConnectedWallet " + ConnectedWallet() );
-        console.log( "ReducedConnectedWallet " + ReduceStringNameWalletAddress() );
-        
-        document.getElementById("ConnectedWallet").innerText = ReducedStringNameWalletAddress() + " Connected";
+        let WalletAddress = ConnectedWallet() + " ";
+        return ( WalletAddress.slice(0,6) + "..." + WalletAddress.slice(-5,-1) );;
     }
