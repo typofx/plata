ac8b33bb71db3c3ffa54a171e6578341f6fa6714
Adam Soares <uarloque@live.com>
2023-04-15 23:08:20 +0100

    Update tokenprices.php

--- tokenprices.php
+++ tokenprices.php
@@ -1,19 +1,23 @@
 <script>
 
-// Y7KBS7GQBHUEQ3CM3JSQK1I69UIGGPDC1J API key 01 Polygonscan
-// 2ABY8VD5TMU2BY758M4P4AYJRDITZTU59U API key 02 Polygonscan
-
 // 0xc298812164bd558268f51cc6e3b8b5daaf0b6341 Plata Token Contract
+// 0x820802Fa8a99901F52e39acD21177b0BE6EE2974 EUROe
+// 0x491a4eB4f1FC3BfF8E1d2FC856a6A46663aD556f BRZ
+// 0x2791bca1f2de4661ed88a30c99a7a9449aa84174 USDC
 
 const PlataTotalSupply = 'https://api.polygonscan.com/api?module=stats&action=tokensupply&contractaddress=0xc298812164bd558268f51cc6e3b8b5daaf0b6341&apikey=Y7KBS7GQBHUEQ3CM3JSQK1I69UIGGPDC1J';
 const PlataLockedSupply = 'https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0xc298812164bd558268f51cc6e3b8b5daaf0b6341&address=0x000000000000000000000000000000000000dead&tag=latest&apikey=Y7KBS7GQBHUEQ3CM3JSQK1I69UIGGPDC1J';
 const MaticLastPrice = 'https://api.polygonscan.com/api?module=stats&action=maticprice&apikey=Y7KBS7GQBHUEQ3CM3JSQK1I69UIGGPDC1J';
 
-const PlataBalanceQuickswapV2PLTMATICPool = 'https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0xc298812164bd558268f51cc6e3b8b5daaf0b6341&address=0x0E145c7637747CF9cfFEF81b6A0317cA3c9671a6&tag=latest&apikey=Y7KBS7GQBHUEQ3CM3JSQK1I69UIGGPDC1J';
-const WMATICbalanceQuickswapV2PLTMATICPool = 'https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270&address=0x0E145c7637747CF9cfFEF81b6A0317cA3c9671a6&tag=latest&apikey=Y7KBS7GQBHUEQ3CM3JSQK1I69UIGGPDC1J';
+const PlataBalanceQuickswapV2PLTMATICPool = 'https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0xc298812164bd558268f51cc6e3b8b5daaf0b6341&address=0x0E145c7637747CF9cfFEF81b6A0317cA3c9671a6&tag=latest&apikey=V28GTTSIS47YEB7IRH36ZQ349KHV4EKVYZ';
+const WMATICbalanceQuickswapV2PLTMATICPool = 'https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270&address=0x0E145c7637747CF9cfFEF81b6A0317cA3c9671a6&tag=latest&apikey=V28GTTSIS47YEB7IRH36ZQ349KHV4EKVYZ';
 
-const BRZBalanceQuickswapV2PLTMATICPool = 'https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0x491a4eB4f1FC3BfF8E1d2FC856a6A46663aD556f&address=0x05487482919f150ACDcA6154Dbd5DF4271fE5910&tag=latest&apikey=2ABY8VD5TMU2BY758M4P4AYJRDITZTU59U';
+const BRZBalanceQuickswapV2PLTMATICPool = 'https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0x491a4eB4f1FC3BfF8E1d2FC856a6A46663aD556f&address=0x05487482919f150ACDcA6154Dbd5DF4271fE5910&tag=latest&apikey=V28GTTSIS47YEB7IRH36ZQ349KHV4EKVYZ';
 const USDCbalanceQuickswapV2PLTMATICPool = 'https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0x2791bca1f2de4661ed88a30c99a7a9449aa84174&address=0x05487482919f150ACDcA6154Dbd5DF4271fE5910&tag=latest&apikey=2ABY8VD5TMU2BY758M4P4AYJRDITZTU59U';
+
+const EUROeBalanceUniswapV3 = 'https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0x820802Fa8a99901F52e39acD21177b0BE6EE2974&address=0x2ccd89fc6ccaa8f49d7fa3d4d7b82b1947f08a49&tag=latest&apikey=2ABY8VD5TMU2BY758M4P4AYJRDITZTU59U';
+const WMATICbalanceUniswapV3 = 'https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270&address=0x2ccd89fc6ccaa8f49d7fa3d4d7b82b1947f08a49&tag=latest&apikey=2ABY8VD5TMU2BY758M4P4AYJRDITZTU59U';
+
 
 let _plataTotalSupply = 0;
 let _plataCirculatingSupply = 0;
@@ -27,10 +31,17 @@ let _PLTUSD = 0;
 let _USDPLT = 0;
 let _PLTBRL = 0;
 let _PLTEUR = 0;
+let _EURUSD = 0;
 
 let _QuickswapUSDC = 0;
 let _QuickswapBRZ = 0;
 let _BRZUSDC = 0;
+
+let _plataMarketCap = 0;
+
+let _UniswapEUROo = 0;
+let _UniswapUSDC = 0;
+let _UniswapWMATIC = 0;
 
 async function getMATICUSD(){
     const response = await fetch(MaticLastPrice);
@@ -41,7 +52,7 @@ async function getMATICUSD(){
     _MATICusd = result.maticusd;
     console.log("MATIC price: " + _MATICusd);
     
-    document.getElementById('txtMATICUSD').textContent = "MATICUSD: " + _MATICusd;
+    document.getElementById('txtMATICUSD').textContent = "MATIC (USD): " + _MATICusd;
 
     } getMATICUSD();
 
@@ -53,7 +64,7 @@ async function getPlataOnPool(){
     _QuickswapPLT = result / 10000;
     
     console.log("Quickswap PLT on Pool: " + _QuickswapPLT);
-    document.getElementById('txtPlataOnPool').textContent = "Quickswap PLT on Pool: " + _QuickswapPLT;
+    //document.getElementById('txtPlataOnPool').textContent = "Quickswap PLT on Pool: " + _QuickswapPLT;
 
 } getPlataOnPool();
 
@@ -83,27 +94,65 @@ async function getUSDConPool(){
 
 } getUSDConPool();
 
+async function getEUROoOnEURUSDCpool(){
+    const response = await fetch(EUROeBalanceUniswapV3);
+    const dataEUROeBalanceUniswapV3 = await response.json();
+    const { result } = dataEUROeBalanceUniswapV3;
+
+    _UniswapEUROo = result / 1000000;
+    //_BRZUSDC = (_QuickswapUSDC / _QuickswapBRZ).toFixed(10);
+    
+    console.log("Uniswap EUROo on EUROoUSDC Pool: " + _UniswapEUROo);
+    //console.log("BRZUSDC: " + _BRZUSDC);
+    //document.getElementById('txtPlataOnPool').textContent = "Quickswap PLT on Pool: " + _QuickswapPLT;
+
+} getEUROoOnEURUSDCpool();
+
+async function getWMATIConEURUSDCpool(){
+    const response = await fetch(WMATICbalanceUniswapV3);
+    const dataWMATICbalanceUniswapV3 = await response.json();
+    const { result } = dataWMATICbalanceUniswapV3;
+
+    _UniswapWMATIC = (result / 1000000000000000000);
+    _UniswapUSDC = _UniswapWMATIC * _MATICusd;
+    _EURUSD = (_UniswapUSDC / _UniswapEUROo).toFixed(10);
+
+    console.log("Uniswap WMATIC on EUROoUSDC Pool: " + _UniswapWMATIC);
+    console.log("Uniswap USDC on EUROoUSDC Pool: " + _UniswapUSDC);
+    console.log("EURoUSDC Price: " + _EURUSD);
+    
+    //console.log("BRZUSDC: " + _BRZUSDC);
+    //document.getElementById('txtPlataOnPool').textContent = "Quickswap PLT on Pool: " + _QuickswapPLT;
+
+} getWMATIConEURUSDCpool();
+
 async function getWMATIConPool(){
     const response = await fetch(WMATICbalanceQuickswapV2PLTMATICPool);
     const dataWMATICbalanceQuickswapV2PLTMATICPool = await response.json();
     const { result } = dataWMATICbalanceQuickswapV2PLTMATICPool;
     
-    _QuickswapLiquidity = (_MATICusd * result / 1000000000000000000).toFixed(10);
-    _QuickswapWMATIC = (result / 1000000000000000000).toFixed(10);
+    _QuickswapLiquidity = (_MATICusd * result / 1000000000000000000).toFixed(12);
+    _QuickswapWMATIC = (result / 1000000000000000000).toFixed(12);
 
     _USDPLT = _QuickswapPLT / _QuickswapLiquidity;
-    _PLTUSD = (1 / _USDPLT).toFixed(10);
-    _PLTBRL = (_PLTUSD / 0.2).toFixed(10);
+    _PLTUSD = (1 / _USDPLT).toFixed(12);
+    _PLTBRL = (_PLTUSD / _BRZUSDC).toFixed(12);
 
     console.log("Quickswap wMATIC on Pool: " + _QuickswapWMATIC);
     console.log("Quickswap Liquidity on Pool: " + _QuickswapLiquidity);
     
+    _PLTEUR = (_PLTUSD / _EURUSD).toFixed(12);
+    
     console.log("USDPLT Price: " + _USDPLT);
+    console.log("PLTEUR Price: " + _PLTEUR);
+    
     console.log("PLTUSD Price: " + _PLTUSD);
     console.log("PLTBRL Price: " + _PLTBRL);
+    
 
-    document.getElementById('txtWMATIConPool').textContent = "Quickswap wMATIC on Pool: " + _QuickswapWMATIC;
-    document.getElementById('txtUSDonPool').textContent = "Quickswap Liquidity on Pool: " + _QuickswapLiquidity;
+    //document.getElementById('txtWMATIConPool').textContent = "Quickswap wMATIC on Pool: " + _QuickswapWMATIC;
+    //document.getElementById('txtUSDonPool').textContent = "Quickswap Liquidity on Pool: " + _QuickswapLiquidity;
+    document.getElementById('txtPLTEUR').textContent = "Plata Token (EUR): " + _PLTEUR;
     document.getElementById('txtPLTUSD').textContent = "Plata Token (USD): " + _PLTUSD;
     document.getElementById('txtPLTBRL').textContent = "Plata Token (BRL): " + _PLTBRL;
 
@@ -115,10 +164,10 @@ async function getPlataTotalSupply(){
     const { result } = dataPlataTotalSupply;
 
     _plataTotalSupply = result / 10000;
-    
+
     console.log("Plata Total Supply : " + _plataTotalSupply);
-    
-    document.getElementById('txtPlataTotalSupply').textContent = "Plata Total Suppy: " + _plataTotalSupply;
+
+    //document.getElementById('txtPlataTotalSupply').textContent = "Plata Total Suppy: " + _plataTotalSupply;
 
 } getPlataTotalSupply();
 
@@ -131,26 +180,30 @@ async function getCirculatingSupply(){
     _plataCirculatingSupply = (result - _plataTotalSupply) / 10000;
     _plataLockedSupply = result / 10000;
     
+    _plataMarketCap = ( _plataCirculatingSupply * _PLTUSD).toFixed(3);
+
     console.log("Plata Circulating Supply : " + _plataCirculatingSupply);
     console.log("Plata Locked Supply : " + _plataLockedSupply);
+    console.log("Plata Marketcap : " + _plataMarketCap);    
 
-    document.getElementById('txtPlataCirculatingSupply').textContent = "Plata Circulating Suppy: " + _plataCirculatingSupply;
-    document.getElementById('txtPlataLockedgSupply').textContent = "Plata Locked Suppy: " + _plataLockedSupply;
+    document.getElementById('txtPlataCirculatingSupply').textContent = "Plata Circulating Suppy: " + _plataCirculatingSupply + " PLT";
+    document.getElementById('txtPlataMarketcap').textContent = "Plata Marketcap: " + _plataMarketCap + " USD";
+    //document.getElementById('txtPlataLockedgSupply').textContent = "Plata Locked Suppy: " + _plataLockedSupply;
 
 } getCirculatingSupply();
+
+
+
 
 
 </script>
 
 <body>
 
-<p id="txtPlataTotalSupply"/>
 <p id="txtPlataCirculatingSupply"/>
-<p id="txtPlataLockedgSupply"/>
+<p id="txtPlataMarketcap"/>
 <p id="txtMATICUSD"/>
-<p id="txtPlataOnPool"/>
-<p id="txtWMATIConPool"/>
-<p id="txtUSDonPool"/>
+<p id="txtPLTEUR"/>
 <p id="txtPLTUSD"/>
 <p id="txtPLTBRL"/>
 
