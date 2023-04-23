2ba25038fbd4513c8f35a7227d5bda8837a81a52
Adam Soares <uarloque@live.com>
2023-04-23 18:16:11 +0100

    Update tokenprices.php

--- tokenprices.php
+++ tokenprices.php
@@ -20,6 +20,9 @@ const USDCbalanceQuickswap = 'https://api.polygonscan.com/api?module=account&act
 let _plataTotalSupply = Number(0);
 let _plataCirculatingSupply = Number(0);
 let _plataLockedSupply = Number(0);
+let _plataMarketCapUSDOUT = Number(0);
+let _plataMarketCapBRLOUT = Number(0);
+let _plataMarketCapEUROUT = Number(0);
 
 let _MATICusd = Number(0);
 let _MATICeur = Number(0);
@@ -49,15 +52,16 @@ let _UniswapUSDC = Number(0);
 let _UniswapUSDT = Number(0);
 
 let i = 0;
+let interv = Number(6666);
 
-    setInterval(getWMATIConPool, 10000);
-    setInterval(getCirculatingSupply, 10000);
-    setInterval(getPlataOnPool, 10000);
-    setInterval(getBRZonPool, 10000);
-    setInterval(getUSDConPool, 10000);
-    setInterval(getUSDTpool, 10000);
-    setInterval(getEUROpool, 10000);
-    setInterval(getMATICprice, 10000);
+    setInterval(getWMATIConPool, interv);
+    setInterval(getCirculatingSupply, interv);
+    setInterval(getPlataOnPool, interv);
+    setInterval(getBRZonPool, interv);
+    setInterval(getUSDConPool, interv);
+    setInterval(getUSDTpool, interv);
+    setInterval(getEUROpool, interv);
+    setInterval(getMATICprice, interv);
 
 async function getPlataTotalSupply(){
     const responsePlataTotalSupply = await fetch(PlataTotalSupply);
@@ -81,9 +85,10 @@ async function getCirculatingSupply(){
     do {
         _plataLockedSupply = Number( _result * 0.0001 );
         _plataCirculatingSupply = Number( _plataTotalSupply - _plataLockedSupply ).toFixed(5);
+        if (_plataCirculatingSupply > 0 || !isNaN(_plataCirculatingSupply) || isFinite(_plataCirculatingSupply)) break;
         //console.log("Plata Circulating Supply : " + parseFloat(_plataCirculatingSupply));
         i=i+1;
-    } while (_plataCirculatingSupply === 0 || isNaN(_plataCirculatingSupply) || !isFinite(_plataCirculatingSupply) || i<5);
+    } while (_plataCirculatingSupply <= 0 || isNaN(_plataCirculatingSupply) || !isFinite(_plataCirculatingSupply) || i<5);
     
     return 0;
     
@@ -123,10 +128,20 @@ async function getUSDConPool(){
     const { result } = dataUSDCbalanceQuickswap;
     let _result = Number(result);
 
-    _QuickswapUSDC = parseFloat(_result /10e6).toFixed(5);
-
-    _BRZUSDC = (parseFloat(_QuickswapUSDC / _QuickswapBRZ)).toFixed(5);
-
+        i=0;
+    do {
+        _QuickswapUSDC = parseFloat(_result /10e6).toFixed(5);
+        if (_QuickswapUSDC > 0 || !isNaN(_QuickswapUSDC) || isFinite(_QuickswapUSDC)) break;
+        i=i+1;
+    } while (_QuickswapUSDC <= 0 || isNaN(_QuickswapUSDC) || !isFinite(_QuickswapUSDC) || i<5);
+    
+        i=0;
+    do {
+        _BRZUSDC = (parseFloat(_QuickswapUSDC / _QuickswapBRZ)).toFixed(5);
+        if (_BRZUSDC > 0 || !isNaN(_BRZUSDC) || isFinite(_BRZUSDC)) break;
+        i=i+1;
+    } while (_BRZUSDC <= 0 || isNaN(_BRZUSDC) || !isFinite(_BRZUSDC) || i<5);
+    
     //console.log("Quickswap USDC on Pair Pool: " + _QuickswapUSDC);
     //console.log("BRLUSD : " + _BRZUSDC);
     //document.getElementById('txtPlataOnPool').textContent = "Quickswap PLT on Pool: " + _QuickswapPLT;
@@ -173,32 +188,47 @@ async function getMATICprice(){
     i=0;
     do {
         _MATICusd = parseFloat(_result).toFixed(3);
-        if (_MATICusd != 0 || !isNaN(_MATICusd) || isFinite(_MATICusd)) break;
+        if (_MATICusd > 0 || !isNaN(_MATICusd) || isFinite(_MATICusd)) break;
         i=i+1;
-    } while (_MATICusd === 0 || isNaN(_MATICusd) || !isFinite(_MATICusd) || i<5);
+    } while (_MATICusd > 0 || isNaN(_MATICusd) || !isFinite(_MATICusd) || i<5);
     
     i=0;
     do {
          _MATICeur = parseFloat(_MATICusd/_EURUSD).toFixed(3);
-         if (_MATICeur != 0 || !isNaN(_MATICeur) || isFinite(_MATICeur)) break;
+         if (_MATICeur > 0 || !isNaN(_MATICeur) || isFinite(_MATICeur)) break;
          i=i+1;
-    } while (_MATICeur === 0 || isNaN(_MATICeur) || !isFinite(_MATICeur) || i<5);
+    } while (_MATICeur <= 0 || isNaN(_MATICeur) || !isFinite(_MATICeur) || i<5);
     
     i=0;
     do {
         _MATICbrl = parseFloat(_MATICusd/_BRZUSDC).toFixed(3);
-        if (_MATICbrl != 0 || !isNaN(_MATICbrl) || isFinite(_MATICbrl)) break;
+        if (_MATICbrl > 0 || !isNaN(_MATICbrl) || isFinite(_MATICbrl)) break;
         i=i+1;
-    } while (_MATICbrl === 0 || isNaN(_MATICbrl) || !isFinite(_MATICbrl) || i<5);
+    } while (_MATICbrl <= 0 || isNaN(_MATICbrl) || !isFinite(_MATICbrl) || i<5);
     
-    console.log("MATIC (USD) : " + parseFloat(_MATICusd) );
-    console.log("MATIC (EUR) : " + parseFloat(_MATICeur) );
-    console.log("MATIC (BRL) : " + parseFloat(_MATICbrl) );
+    //console.log("MATIC (USD) : " + parseFloat(_MATICusd) );
+    //console.log("MATIC (EUR) : " + parseFloat(_MATICeur) );
+    //console.log("MATIC (BRL) : " + parseFloat(_MATICbrl) );
     //document.getElementById('txtMATICUSD').textContent = "MATIC (USD): " + _MATICusd;
     
     return _MATICusd;
     
 } getMATICprice();
+
+function USDcurrency() {
+    document.getElementById('txtPAIR').textContent = "$ " + _PLTUSD;
+    document.getElementById('numTokenMarketcap').textContent = " $ " + _plataMarketCapUSDOUT;
+}
+
+function EURcurrency() {
+    document.getElementById('txtPAIR').textContent = "€ " + _PLTEUR;
+    document.getElementById('numTokenMarketcap').textContent = " € " + _plataMarketCapEUROUT;
+}
+
+function BRLcurrency() {
+    document.getElementById('txtPAIR').textContent = "R$ " + _PLTBRL;
+    document.getElementById('numTokenMarketcap').textContent = " R$ " +  _plataMarketCapBRLOUT;
+}
 
 async function getWMATIConPool(){
     const response = await fetch(WMATICbalanceQuickswapV2PLTMATICPool);
@@ -210,9 +240,9 @@ async function getWMATIConPool(){
         do {
             _QuickswapWMATIC = parseFloat( _result * 1e-18).toFixed(5);
             //console.log("Quickswap wMATIC on Pool: " + _QuickswapWMATIC);
-            if (_QuickswapWMATIC != 0 || !isNaN(_QuickswapWMATIC) || isFinite(_QuickswapWMATIC)) break;
+            if (_QuickswapWMATIC > 0 || !isNaN(_QuickswapWMATIC) || isFinite(_QuickswapWMATIC)) break;
             i = i+1;   
-        } while (_QuickswapWMATIC === 0 || isNaN(_QuickswapWMATIC) || !isFinite(_QuickswapWMATIC) || i<5);
+        } while (_QuickswapWMATIC <= 0 || isNaN(_QuickswapWMATIC) || !isFinite(_QuickswapWMATIC) || i<5);
         
         _QuickswapLiquidity = Number(_MATICusd*_QuickswapWMATIC).toFixed(3);
         //console.log("Quickswap Liquidity on Pool: " + _QuickswapLiquidity + " USD");
@@ -221,60 +251,75 @@ async function getWMATIConPool(){
         do {
             _USDPLT = parseFloat((_QuickswapPLT)/parseFloat(_QuickswapLiquidity)).toFixed(5);
             //console.log("USDPLT : " + _USDPLT);
-            if (_USDPLT != 0 || !isNaN(_USDPLT) || isFinite(_USDPLT)) break;
+            if (_USDPLT > 0 || !isNaN(_USDPLT) || isFinite(_USDPLT)) break;
             i = i+1;            
-        } while (_USDPLT === 0 || isNaN(_USDPLT) || !isFinite(_USDPLT));
+        } while (_USDPLT <= 0 || isNaN(_USDPLT) || !isFinite(_USDPLT));
         
         i = 0;
         do {
             _PLTUSD = parseFloat(1/_USDPLT).toFixed(10);
             //console.log("PLTUSD : " + _PLTUSD);
-            if (_PLTUSD != 0 || !isNaN(_PLTUSD) || isFinite(_PLTUSD)) break;
+            if (_PLTUSD > 0 || !isNaN(_PLTUSD) || isFinite(_PLTUSD)) break;
             i = i+1;
-        } while (_PLTUSD === 0 || isNaN(_PLTUSD) || !isFinite(_PLTUSD) || i<5);
+        } while (_PLTUSD <= 0 || isNaN(_PLTUSD) || !isFinite(_PLTUSD) || i<5);
         
         i = 0;
         do {
             _PLTEUR = parseFloat(_PLTUSD/_EURUSD).toFixed(10);
             //console.log("PLTEUR : " + _PLTEUR);
-            if (_PLTEUR != 0 || !isNaN(_PLTEUR) || isFinite(_PLTEUR)) break;
+            if (_PLTEUR > 0 || !isNaN(_PLTEUR) || isFinite(_PLTEUR)) break;
             i = i+1;
-        } while (_PLTEUR === 0 || isNaN(_PLTEUR) || !isFinite(_PLTEUR) || i<5);
+        } while (_PLTEUR <= 0 || isNaN(_PLTEUR) || !isFinite(_PLTEUR) || i<5);
         
         i = 0;
         do {
             _PLTBRL = parseFloat(_PLTUSD/_BRZUSDC).toFixed(10);
             //console.log("PLTBRL : " + _PLTBRL);
-            if (_PLTBRL != 0 || !isNaN(_PLTBRL) || isFinite(_PLTBRL)) break;
+            if (_PLTBRL > 0 || !isNaN(_PLTBRL) || isFinite(_PLTBRL)) break;
             i = i+1;
-        } while (_PLTBRL === 0 || isNaN(_PLTBRL) || !isFinite(_PLTBRL) || i<5);
+        } while (_PLTBRL <= 0 || isNaN(_PLTBRL) || !isFinite(_PLTBRL) || i<5);
         
-        _plataMarketCap = parseFloat(_plataCirculatingSupply*_PLTUSD);
-        const _plataMarketCapSTR = _plataMarketCap;
-        const  _plataMarketCapOUT = _plataMarketCapSTR.toLocaleString("en-US");
+        _plataMarketCapUSD = parseFloat(_plataCirculatingSupply*_PLTUSD);
+        _plataMarketCapBRL = parseFloat(_plataCirculatingSupply*_PLTBRL);
+        _plataMarketCapEUR = parseFloat(_plataCirculatingSupply*_PLTEUR);
         
-        console.log(_plataMarketCapOUT);
+        const _plataMarketCapUSDSTR = _plataMarketCapUSD;
+        const _plataMarketCapBRLSTR = _plataMarketCapBRL;
+        const _plataMarketCapEURSTR = _plataMarketCapEUR;
+        
+        _plataMarketCapUSDOUT = _plataMarketCapUSDSTR.toLocaleString("en-US");
+        _plataMarketCapBRLOUT = _plataMarketCapBRLSTR.toLocaleString("en-US");
+        _plataMarketCapEUROUT = _plataMarketCapEURSTR.toLocaleString("en-US");
+        
+        //console.log("Marketcap (USD)" + _plataMarketCapUSDOUT);
+        //console.log("Marketcap (BRL)" + _plataMarketCapBRLOUT);
+        //console.log("Marketcap (EUR)" + _plataMarketCapEUROUT);
         //console.log("Plata Marketcap (USD) : " + parseFloat(_plataMarketCap));
+
+    //document.getElementById('txtTokenCurrency').textContent = "$ ";
+    //document.getElementById('txtTokenPrice').textContent = parseFloat(<?php $txtTokenPriceCurrency ?>);
+    //document.getElementById('txtMarketcap').textContent = "Marketcap : $ " + _plataMarketCapOUT;
         
+    //document.getElementById('txtPlataTotalSupply').textContent = "Plata Total Supply: " + parseFloat(_plataTotalSupply) + " PLT";
+    //document.getElementById('txtPlataCirculatingSupply').textContent = "Plata Circulating Supply: " + parseFloat(_plataCirculatingSupply) + " PLT";
+    
+    //document.getElementById('txtPlataMarketcap').textContent = "Plata Marketcap: " + parseFloat(_plataMarketCap) + " USD";
+    
+    //const d = new Date();
+    //console.log("Time : "+ d);
     
     document.getElementById('txtTokenName').textContent = "Plata Token";
     document.getElementById('txtTokenSymbol').textContent = " (PLT)";
-    document.getElementById('txtTokenCurrency').textContent = "$ ";
-    document.getElementById('txtTokenPrice').textContent = parseFloat(_PLTUSD);
-    document.getElementById('txtMarketcap').textContent = "Marketcap : $ " + _plataMarketCapOUT;
     
-    //document.getElementById('txtPlataTotalSupply').textContent = "Plata Total Supply: " + parseFloat(_plataTotalSupply) + " PLT";
-    //document.getElementById('txtPlataCirculatingSupply').textContent = "Plata Circulating Supply: " + parseFloat(_plataCirculatingSupply) + " PLT";
-    //document.getElementById('txtPLTUSD').textContent = "Plata Token (USD): " + parseFloat(_PLTUSD);
-    //document.getElementById('txtPLTEUR').textContent = "Plata Token (EUR): " + parseFloat(_PLTEUR);
-    //document.getElementById('txtPLTBRL').textContent = "Plata Token (BRL): " + parseFloat(_PLTBRL);
-    //document.getElementById('txtPlataMarketcap').textContent = "Plata Marketcap: " + parseFloat(_plataMarketCap) + " USD";
+    if (document.getElementById("txtCurrencyEnv").textContent === " (USD)") USDcurrency();
+    if (document.getElementById("txtCurrencyEnv").textContent === " (EUR)") EURcurrency();
+    if (document.getElementById("txtCurrencyEnv").textContent === " (BRL)") BRLcurrency();
     
-    const d = new Date();
-    console.log("Time : "+ d);
     //document.getElementById("txtHour").innerHTML = d.toLocaleTimeString();
     
 } getWMATIConPool();
+
+USDcurrency();
 
 /*
 <script> </script>
