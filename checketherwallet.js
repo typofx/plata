fe3ea51bb5b3b5bc67d7cde0aa760ff852a69bbd
Adam Soares <uarloque@live.com>
2023-05-11 00:28:12 +0100

    Create checketherwallet.js
    Code to check if provided data is a valid ether wallet or not

---
+++ checketherwallet.js
@@ -0,0 +1,22 @@
+//Code to check if provided data is a valid ether wallet or not
+
+<script src="https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js"></script>
+
+<script>    
+    function exec(){
+        let address = document.getElementById("txtEtherWallet").value;
+        let result = Web3.utils.isAddress(address);
+        console.log(result);  // => true?
+    }
+</script>
+
+<html>
+    <head>
+    </head>
+    <body>
+        <label for="fname">Ether Wallet:</label>
+        <input type="text" id="txtEtherWallet" name="txtEtherWallet"><br><br>
+        <button type="button" onclick="exec()">Submit</button>
+    </body>   
+</html>
+
