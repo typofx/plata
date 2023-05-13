84ab8575f22b702d83af392e683d3c95eb5be8d6
Adam Soares <uarloque@live.com>
2023-05-13 23:25:23 +0100

    Delete script directory

--- script/copyContract.js
+++
@@ -1,9 +0,0 @@
-// <button onclick = "copyContract()">Text</button>
-
-function copyContract() {
-  var copyText = "0xc58A1559b566863668A8C7316da00faC01202300";
-  /* Copy the text inside the text field */
-  navigator.clipboard.writeText(copyText);
-  /* Alert the copied text */
-  alert("$PLT Plata Contract Hash Address Copied! " copyText);
-}
