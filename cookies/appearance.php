d5aa6aada42560b80ae1a583ccc396a10b74378a
adriel-lab <74466153+adriel-lab@users.noreply.github.com>
2023-05-18 09:30:06 -0300

    Update appearance.php

--- cookies/appearance.php
+++ cookies/appearance.php
@@ -1,23 +1,18 @@
 <?php
-if(isset($_COOKIE['appearance'])) {
+if (isset($_COOKIE['appearance'])) {
     $appearance = $_COOKIE['appearance'];
 } else {
     $appearance = 'off'; // Definir valor padrão
 }
 
-if(isset($_POST['change_appearance'])) {
-    if($appearance == 'off') {
+if (isset($_POST['change_appearance'])) {
+    if ($appearance == 'off') {
         $appearance = 'on';
     } else {
         $appearance = 'off';
     }
 
-    setcookie('appearance', $appearance, time() + (86400 * 30), '/'); // Definir cookie por 30 dias
-}
-
-function setCookieValue($value) {
-    setcookie('appearance', $value, time() + (86400 * 30), '/'); // Definir cookie por 30 dias
-    $_COOKIE['appearance'] = $value; // Atualizar o valor do cookie na variável $_COOKIE
+    setcookie('appearance', $appearance, time() + (86400 * 30), '/', '', true, true); // Definir cookie por 30 dias com Secure e HttpOnly ativados
 }
 ?>
 
@@ -26,7 +21,7 @@ window.addEventListener('DOMContentLoaded', function() {
     const elem = document.getElementById("info");
     const body = document.body;
 
-    if (elem.innerText == "on") {
+    if (elem.innerText === "on") {
         body.style.backgroundColor = "#000000"; // Fundo escuro
         body.style.color = "#ffffff"; // Texto branco
     } else {
@@ -37,25 +32,39 @@ window.addEventListener('DOMContentLoaded', function() {
 
 function setCookie(value) {
     // Enviar uma solicitação para atualizar o cookie usando PHP
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "", true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send("change_appearance=1");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                // Atualizar o valor do cookie na página sem recarregar
-                setCookieValue(value);
-            }
+    fetch("", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: "change_appearance=1"
+    }).then(function(response) {
+        if (response.ok) {
+            // Atualizar o valor do cookie na página sem recarregar
+            setCookieValue(value);
         }
-    };
+    }).catch(function(error) {
+        console.log("Erro ao atualizar o cookie:", error);
+    });
+}
+
+function setCookieValue(value) {
+    fetch("", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: "set_cookie_value=" + encodeURIComponent(value)
+    }).catch(function(error) {
+        console.log("Erro ao atualizar o valor do cookie:", error);
+    });
 }
 
 function changeAppearance() {
     const elem = document.getElementById("info");
     const body = document.body;
 
-    if (elem.innerText == "on") {
+    if (elem.innerText === "on") {
         elem.innerText = "off";
         body.style.backgroundColor = "#ffffff"; // Mudar para fundo claro
         body.style.color = "#000000"; // Mudar para cor do texto preta
@@ -70,6 +79,7 @@ function changeAppearance() {
 </script>
 
 <body>
-<h1 id="info"><?php echo $appearance; ?></h1>
+<h1 id="info"><?php echo htmlspecialchars($appearance); ?></h1>
 <button type="button" onclick="changeAppearance()">Button</button>
 </body>
+
