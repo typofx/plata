2e1b24166c6a93443972957decd5bab7d97fd267
Adam Soares <uarloque@live.com>
2023-05-26 11:36:11 +0100

    Update split.php

--- mobile/split.php
+++ mobile/split.php
@@ -1,44 +1,55 @@
 <!DOCTYPE html>
 <html>
 <style>
-table, th, td {
-  border:1px solid black;
+.split-table {
+    border-collapse: collapse;
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
 }
+
+/*table, tr,td {
+    border:1px solid black;
+}*/
+
+.center-img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 70%;
+}
+
 </style>
-<body>
 
-<h2>Plata Token Initial Split (2022)</h2>
+<section>
+<h2>Initial Split (2022)</h2>
 
-<table style="width:360px">
+<table class="split-table"> 
   <tr>
-    <td>IMAGE</td>
+    <td><img class="center-img" src="https://www.plata.ie/images/token-split-chart-2022.svg"><br></td>
   </tr>
   <tr>
-    <td>10% Platform Operational Costs</td>
+    <td><img src="https://www.plata.ie/images/sq01.svg"> Platform Operational ( 10% )</td>
   </tr>
   <tr>
-    <td>20% Legal Support</td>
+    <td><img src="https://www.plata.ie/images/sq02.svg"> Legal Support ( 20% )</td>
   </tr>
   <tr>
-    <td>5% Decentralized Management Costs</td>
+    <td><img src="https://www.plata.ie/images/sq03.svg"> Decentralized Management ( 5% )</td>
   </tr>
   <tr>
-    <td>5% Remuneration to Project Mentors</td>
+    <td><img src="https://www.plata.ie/images/sq04.svg"> Expenses to Project Mentors ( 5% )</td>
   </tr>
   <tr>
-    <td>20% Marketing and Promotion Costs</td>
+    <td><img src="https://www.plata.ie/images/sq05.svg"> Marketing and Promotion ( 20% )</td>
   </tr>
   <tr>
-    <td>40% Reserve Fund</td>
+    <td><img src="https://www.plata.ie/images/sq06.svg"> Reserve Fund ( 40% )</td>
+  </tr>
+    <tr>
+    <td><br></td>
   </tr>
 </table>
 
-</body>
-</html>
-
-
-
- 
- 
-
-
+</section>
