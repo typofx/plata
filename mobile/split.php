053e4ab8f02b49d43428acf2e05feb4c4ff6133a
Adam Soares <uarloque@live.com>
2023-05-26 10:07:52 +0100

    Create split.php

---
+++ mobile/split.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<style>
+table, th, td {
+  border:1px solid black;
+}
+</style>
+<body>
+
+<h2>Plata Token Initial Split (2022)</h2>
+
+<table style="width:360px">
+  <tr>
+    <td>IMAGE</td>
+  </tr>
+  <tr>
+    <td>10% Platform Operational Costs</td>
+  </tr>
+  <tr>
+    <td>20% Legal Support</td>
+  </tr>
+  <tr>
+    <td>5% Decentralized Management Costs</td>
+  </tr>
+  <tr>
+    <td>5% Remuneration to Project Mentors</td>
+  </tr>
+  <tr>
+    <td>20% Marketing and Promotion Costs</td>
+  </tr>
+  <tr>
+    <td>40% Reserve Fund</td>
+  </tr>
+</table>
+
+</body>
+</html>
+
+
+
+ 
+ 
+
+
