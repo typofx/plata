15cd9b5fd9b8ae03c59a98afd8f7b0ac68decf43
Adam Soares <uarloque@live.com>
2023-05-27 11:53:59 +0100

    Update split.php

--- mobile/split.php
+++ mobile/split.php
@@ -9,6 +9,10 @@
     text-align: center;
 }
 
+.invisibled {
+    display: none;
+}
+
 /*table, tr,td {
     border:1px solid black;
 }*/
@@ -20,35 +24,96 @@
   width: 70%;
 }
 
+.button-year {
+    width: 120px;
+    height: 40px;
+    display: block;
+    background-color: #4A0086;
+    border-radius: 6px; 
+    border: 0px solid #3D3D3D;
+    color: white; 
+    font-size: 15px; 
+    cursor: pointer; 
+    transition: 0.3s; 
+    font-family: 'Montserrat';
+    text-align: center;
+}
+
+.button-year:hover {
+    background-color: #8247E5; 
+    border-radius: 6px; 
+    color: white;
+    transition: 0.3s;
+} 
+
 </style>
 
-<section>
-<h2>Initial Split (2022)</h2>
+<section id="AnchorInitialSplit">
+<br>
+<h2><?php echo $txtInitialSplit?></h2>
 
-<table class="split-table"> 
+<table id="tbInitialSplit" class="split-table"> 
   <tr>
     <td><img class="center-img" src="https://www.plata.ie/images/token-split-chart-2022.svg"><br></td>
   </tr>
   <tr>
-    <td><img src="https://www.plata.ie/images/sq01.svg"> Platform Operational ( 10% )</td>
+    <td><img src="https://www.plata.ie/images/sq01.svg">  <?php echo $txtPlatform?></td>
   </tr>
   <tr>
-    <td><img src="https://www.plata.ie/images/sq02.svg"> Legal Support ( 20% )</td>
+    <td><img src="https://www.plata.ie/images/sq02.svg">  <?php echo $txtLegalSupport?></td>
   </tr>
   <tr>
-    <td><img src="https://www.plata.ie/images/sq03.svg"> Decentralized Management ( 5% )</td>
+    <td><img src="https://www.plata.ie/images/sq03.svg">  <?php echo $txtDecentralized?></td>
   </tr>
   <tr>
-    <td><img src="https://www.plata.ie/images/sq04.svg"> Expenses to Project Mentors ( 5% )</td>
+    <td><img src="https://www.plata.ie/images/sq04.svg">  <?php echo $txtExpenses?></td>
   </tr>
   <tr>
-    <td><img src="https://www.plata.ie/images/sq05.svg"> Marketing and Promotion ( 20% )</td>
+    <td><img src="https://www.plata.ie/images/sq05.svg">  <?php echo $txtMarketing?></td>
   </tr>
   <tr>
-    <td><img src="https://www.plata.ie/images/sq06.svg"> Reserve Fund ( 40% )</td>
+    <td><img src="https://www.plata.ie/images/sq06.svg">  <?php echo $txtReserve?></td>
   </tr>
     <tr>
     <td><br></td>
+  </tr>
+</table>
+
+<table id="tbInitialSplit" class="split-table invisibled">
+  <tr>
+    <td><img class="center-img" src="https://www.plata.ie/images/token-split-chart-2022.svg"><br></td>
+  </tr>
+  <tr>
+    <td><img src="https://www.plata.ie/images/sq01.svg">  Null: 0x00...dEaD 49%</td>
+  </tr>
+  <tr>
+    <td><img src="https://www.plata.ie/images/sq02.svg">  Typo FX: Wallets 26%</td>
+  </tr>
+  <tr>
+    <td><img src="https://www.plata.ie/images/sq03.svg">  Uniswap V3 5%</td>
+  </tr>
+  <tr>
+    <td><img src="https://www.plata.ie/images/sq04.svg">  Quickswap DEX 5%</td>
+  </tr>
+  <tr>
+    <td><img src="https://www.plata.ie/images/sq05.svg">  SushiSwap V2 5%</td>
+  </tr>
+  <tr>
+    <td><img src="https://www.plata.ie/images/sq06.svg">  Promotional Giveaway 5%</td>
+  </tr>
+  <tr>
+    <td><img src="https://www.plata.ie/images/sq06.svg">  AirDrop dApp 4%</td>
+  </tr>
+    <tr>
+    <td><br></td>
+  </tr>
+</table>
+
+<table style="width:100%">
+  <tr>
+    <td><button class="button-year" onclick="show2022()">2022</button></td>
+    <td><button class="button-year" onclick="show2023()">2023</button></td>
+    <td><button class="button-year" onclick="showNFT()">NFT</button></td>
   </tr>
 </table>
 
