e27e7d18169ac58cc8b7724d00e1bb7b4a83bae1
MilenaAudrey <72368337+MilenaAudrey@users.noreply.github.com>
2023-05-31 20:00:03 -0300

    Update listing.php

--- mobile/listing.php
+++ mobile/listing.php
@@ -4,25 +4,30 @@
 
       <table class="listing-table">
         <tr>
-          <td><a href="https://polygon.poocoin.app/tokens/0xc298812164bd558268f51cc6e3b8b5daaf0b6341" target="_blank"><img src="https://www.plata.ie/images/poocoinLogo.svg" onmouseover="this.src='https://www.plata.ie/images/poocoinLogoOR.svg';" onmouseout="this.src='https://www.plata.ie/images/poocoinLogo.svg';"/></a></td>
-          <td><a href="https://nomics.com/assets/plt9-plata-v2" alt="Nomics" target="_blank"><img src="https://www.plata.ie/images/nomicsLogo.svg" onmouseover="this.src='https://www.plata.ie/images/nomicsLogoOR.svg';" onmouseout="this.src='https://www.plata.ie/images/nomicsLogo.svg';"/></a></td>
+          <td><a href="https://polygon.poocoin.app/tokens/0xc298812164bd558268f51cc6e3b8b5daaf0b6341" target="_blank"><img src="https://www.plata.ie/images/poocoinLogo.svg" ></a></td>
+
+          <td><a href="https://www.geckoterminal.com/polygon_pos/pools/0x5d64b0ec81a0648f52be7a50267bf8d1d0ffb161" alt="XXX" target="_blank"><img src="https://www.plata.ie/images/geckoTerminalLogo.svg"></a></td>
         </tr>
         <tr>
-          <td><a href="https://dexscreener.com/polygon/0x0e145c7637747cf9cffef81b6a0317ca3c9671a6" alt="DEX Screener" target="_blank"><img src="https://www.plata.ie/images/dexScreenerLogo.svg" onmouseover="this.src='https://www.plata.ie/images/dexScreenerLogoOR.svg';" onmouseout="this.src='https://www.plata.ie/images/dexScreenerLogo.svg';"/></a></td>
-          <td><a href="https://www.dextools.io/app/en/polygon/pair-explorer/0x0e145c7637747cf9cffef81b6a0317ca3c9671a6" alt="DEXtools" target="_blank"><img src="https://www.plata.ie/images/dextoolsLogo.svg" onmouseover="this.src='https://www.plata.ie/images/dextoolsLogoOR.svg';" onmouseout="this.src='https://www.plata.ie/images/dextoolsLogo.svg';"/></a></td>
+
+          <td><a href="https://dexscreener.com/polygon/0x0e145c7637747cf9cffef81b6a0317ca3c9671a6" alt="DEX Screener" target="_blank"><img src="https://www.plata.ie/images/dexScreenerLogo.svg" ></a></td>
+
+          <td><a href="https://www.dextools.io/app/en/polygon/pair-explorer/0x0e145c7637747cf9cffef81b6a0317ca3c9671a6" alt="DEXtools" target="_blank"><img src="https://www.plata.ie/images/dextoolsLogo.svg" ></a></td>
         </tr>
         <tr>
-          <td><a href="https://bitkeep.com/en/swap/matic/0xc298812164bd558268f51cc6e3b8b5daaf0b6341" alt="BitKeep" target="_blank"><img src="https://www.plata.ie/images/bitKeepLogo.svg" onmouseover="this.src='https://www.plata.ie/images/bitKeepLogoOR.svg';" onmouseout="this.src='https://www.plata.ie/images/bitKeepLogo.svg';"/></a></td>
-          <td><a href="https://tokensniffer.com/token/poly/0xc298812164bd558268f51cc6e3b8b5daaf0b6341" alt="Token Sniffer" target="_blank"><img src="https://www.plata.ie/images/tokenSnifferLogo.svg" onmouseover="this.src='https://www.plata.ie/images/tokenSnifferLogoOR.svg';" onmouseout="this.src='https://www.plata.ie/images/tokenSnifferLogo.svg';"/></a></td>
+
+          <td><a href="https://bitkeep.com/en/swap/matic/0xc298812164bd558268f51cc6e3b8b5daaf0b6341" alt="BitKeep" target="_blank"><img src="https://www.plata.ie/images/bitKeepLogo.svg" ></a></td>
+
+          <td><a href="https://tokensniffer.com/token/poly/0xc298812164bd558268f51cc6e3b8b5daaf0b6341" alt="Token Sniffer" target="_blank"><img src="https://www.plata.ie/images/tokenSnifferLogo.svg" ></a></td>
         </tr>
         <tr>
-          <td><a href="https://www.geckoterminal.com/polygon_pos/pools/0x5d64b0ec81a0648f52be7a50267bf8d1d0ffb161" alt="XXX" target="_blank"><img src="https://www.plata.ie/images/geckoTerminalLogo.svg" onmouseover="this.src='https://www.plata.ie/images/geckoTerminalLogoOR.svg';" onmouseout="this.src='https://www.plata.ie/images/geckoTerminalLogo.svg';"/></a></td>
-          <td><a href="https://gopluslabs.io/token-security/137/0xc298812164bd558268f51cc6e3b8b5daaf0b6341" alt="XXX" target="_blank"><img src="https://www.plata.ie/images/goplusLogo.svg" onmouseover="this.src='https://www.plata.ie/images/goplusLogoOR.svg';" onmouseout="this.src='https://www.plata.ie/images/goplusLogo.svg';"/></a></td>
+
+          <td><a href="https://top100token.com/address/0xc298812164bd558268f51cc6e3b8b5daaf0b6341" alt="Top100Token" target="_blank"><img src="https://www.plata.ie/images/top100TokenLogo.svg" ></a></td>
+
+          <td><a href="https://gopluslabs.io/token-security/137/0xc298812164bd558268f51cc6e3b8b5daaf0b6341" alt="XXX" target="_blank"><img src="https://www.plata.ie/images/goplusLogo.svg"></a></td>
         </tr>
-        <tr>
-          <td><a href="https://top100token.com/address/0xc298812164bd558268f51cc6e3b8b5daaf0b6341" alt="Top100Token" target="_blank"><img src="https://www.plata.ie/images/top100TokenLogo.svg" onmouseover="this.src='https://www.plata.ie/images/top100TokenLogoOR.svg';" onmouseout="this.src='https://www.plata.ie/images/top100TokenLogo.svg';"/></a></td>
-          <td></td>
-        </tr>
+        
+
       </table>
         <br>
         <table class="listing-table">
