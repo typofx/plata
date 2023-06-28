63356bae8448cc19a3fc3762353fa7ec820d8b24
Lucas Carvalho Santos <95755190+luke-oak@users.noreply.github.com>
2023-06-28 17:34:52 -0300

    Update team.php

--- desktop/team.php
+++ desktop/team.php
@@ -1,1 +1,116 @@
-...
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:ital,wght@0,400;0,700;1,400;1,700&display=swap">
+
+    <title>Teste</title>
+</head>
+<body>
+    <h1>Testando Team</h1>
+    <br>
+    <table class="tb-members">
+        <tr>
+            <td class="td-tb-members">
+                <img class="img-members" src="https://www.plata.ie/images/profileAdamSoares.jpg" alt="Foto">
+                <p class="member-roles">CTO & MECHANICAL ENGINEER</p>
+                <h2 class="member-names">Adam Soares</h2>
+                <div class="div-icons">
+                    <a class="link-social-icons align-right" title="facebook" target="_blank" href="https://facebook.com/uarloque">
+                        <img class="social-icons align-right" src="https://www.plata.ie/images/social-purple-facebook.png">
+                    </a>
+                    <a class="link-social-icons align-right" title="github" target="_blank" href="https://github.com/uarloque">
+                        <img class="social-icons align-right" src="https://www.plata.ie/images/social-purple-github.png">
+                    </a>
+                    <a class="link-social-icons align-right" title="linkedin" target="_blank" href="https://www.linkedin.com/in/adam-soares-a44b833a/">
+                        <img class="social-icons align-right" src="https://www.plata.ie/images/social-purple-linkedin.png">
+                    </a>
+                </div>
+            </td>
+            <td class="td-tb-members">
+                <img class="img-members" src="https://www.plata.ie/images/profileThiagoSilva.jpg" alt="Foto">
+                <p class="member-roles">CTO & MECHANICAL ENGINEER</p>
+                <h2 class="member-names">Thiago Bezerra</h2>
+                <div class="div-icons">
+                    <a class="link-social-icons" title="facebook" target="_blank" href="https://www.facebook.com/dublinareal">
+                        <img class="social-icons" src="https://www.plata.ie/images/social-purple-facebook.png">
+                    </a>
+                    <a class="link-social-icons" title="linkedin" target="_blank" href="https://www.linkedin.com/in/thiago-bezerra-da-silva-85a72b46/">
+                        <img class="social-icons" src="https://www.plata.ie/images/social-purple-linkedin.png">
+                    </a>  
+                    <a class="link-social-icons" title="instagram" target="_blank" href="https://www.instagram.com/tchago/">
+                        <img class="social-icons" src="https://www.plata.ie/images/social-purple-instagram.png">
+                    </a>
+                </div>
+            </td>
+            <td class="td-tb-members">
+                <img class="img-members" src="https://www.plata.ie/images/profileGustavoSalles.jpg" alt="Foto">
+                <p class="member-roles">ENGAGEMENT & MARKETING MANAGER</p>
+                <h2 class="member-names">Gustavo Salles</h2>
+                <div class="div-icons">
+                    <a class="link-social-icons" title="email" target="_blank" href="mailto:gustavosalles@plata.ie">
+                        <img class="social-icons" src="https://www.plata.ie/images/social-purple-email.png">
+                    </a>
+                    <a class="link-social-icons" title="linkedin" target="_blank" href="https://www.linkedin.com/in/gustavo-toledo-salles-b44b0941/">
+                        <img class="social-icons" src="https://www.plata.ie/images/social-purple-linkedin.png">
+                    </a>  
+                    <a class="link-social-icons" title="instagram" target="_blank" href="https://instagram.com/gustavostoledo">
+                        <img class="social-icons" src="https://www.plata.ie/images/social-purple-instagram.png">
+                    </a>
+                </div>
+            </td>
+            <td class="td-tb-members">
+                <img class="img-members" src="https://www.plata.ie/images/MilenaAudrey.jpeg" alt="Foto">
+                <p class="member-roles">WEB3 TRAINEE DEVELOPER</p>
+                <h2 class="member-names">Milena Audrey</h2>
+                <div class="div-icons">
+                    <a class="link-social-icons" title="linkedin" target="_blank" href="https://www.linkedin.com/in/milena-n%C3%B3brega-b58034219/">
+                        <img img class="social-icons" src="https://www.plata.ie/images/social-purple-linkedin.png">
+                    </a>
+                    <a class="link-social-icons" title="github" target="_blank" href="https://github.com/MilenaAudrey">
+                        <img img class="social-icons" src="https://www.plata.ie/images/social-purple-github.png">
+                    </a>
+                    <a class="link-social-icons" title="email" target="_blank" href="milenaaudrey@gmail.com">
+                        <img img class="social-icons" src="https://www.plata.ie/images/social-purple-email.png">
+                    </a>
+                </div>
+            </td>
+            <td class="td-tb-members">
+                <img class="img-members" src="https://www.plata.ie/images/profileAdrielDias.jpg" alt="Foto">
+                <p class="member-roles">WEB3 TRAINEE DEVELOPER</p>
+                <h2 class="member-names">Adriel Dias</h2>
+                <div class="div-icons">
+                    <a class="link-social-icons" title="instagram" target="_blank" href="https://www.instagram.com/adrielf_diazz/">
+                        <img class="social-icons" src="https://www.plata.ie/images/social-purple-email.png">
+                    </a>
+                    <a class="link-social-icons" title="facebook" target="_blank" href="https://www.facebook.com/profile.php?id=100011384699745">
+                        <img class="social-icons" src="https://www.plata.ie/images/social-purple-facebook.png">
+                    </a>  
+                    <a class="link-social-icons" title="github" target="_blank" href="https://github.com/adriel-lab">
+                        <img class="social-icons" src="https://www.plata.ie/images/social-purple-github.png">
+                    </a>
+                </div>
+            </td>
+            <td class="td-tb-members">
+                <img class="img-members" src="https://www.plata.ie/images/profileAdrielDias.jpg" alt="Foto">
+                <p class="member-roles">WEB3 TRAINEE DEVELOPER</p>
+                <h2 class="member-names">Adriel Dias</h2>
+                <div class="div-icons">
+                    <a class="link-social-icons" title="instagram" target="_blank" href="https://www.instagram.com/adrielf_diazz/">
+                        <img class="social-icons" src="https://www.plata.ie/images/social-purple-email.png">
+                    </a>
+                    <a class="link-social-icons" title="facebook" target="_blank" href="https://www.facebook.com/profile.php?id=100011384699745">
+                        <img class="social-icons" src="https://www.plata.ie/images/social-purple-facebook.png">
+                    </a>  
+                    <a class="link-social-icons" title="github" target="_blank" href="https://github.com/adriel-lab">
+                        <img class="social-icons" src="https://www.plata.ie/images/social-purple-github.png">
+                    </a>
+                </div>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
