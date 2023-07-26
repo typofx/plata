0d7ec1835137375377fe3484740b6a37818ceb71
Lucas Carvalho Santos <95755190+luke-oak@users.noreply.github.com>
2023-07-25 23:11:21 -0300

    Create mobile-listing.php
    Add mobile listing

---
+++ mobile/en/mobile-listing.php
@@ -0,0 +1,110 @@
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Base Information, ​Countdown $PLT Airdrop ends in, The Project, Do you need more information?, The Roadmap, Meet The Team, ​Best Wallets For $PLT Plata">
+    <meta name="description" content="">
+    
+    <title>$PLT Plata Token for ACTM</title>
+<body>
+    <link rel="stylesheet" href="./style-desktop-listing.css">
+
+    <div class="table-area">
+        <table class="table-listing">
+            <tbody>      
+                <tr> 
+                    <td class="td-listing">
+                      <div class="content-listing">  
+                        <img class="image-content" src="./fundo.jpg">
+                        <div class="description-image">Paisagem</div>
+                      </div>
+                    </td>
+                    <td class="td-listing">
+                        <div class="content-listing">  
+                          <img class="image-content" src="./fundo.jpg">
+                          <div class="description-image">Paisagem</div>
+                        </div>
+                      </td>
+                      <td class="td-listing">
+                        <div class="content-listing">  
+                          <img class="image-content" src="./fundo.jpg">
+                          <div class="description-image">Paisagem</div>
+                        </div>
+                      </td>
+                      <td class="td-listing">
+                        <div class="content-listing">  
+                          <img class="image-content" src="./fundo.jpg">
+                          <div class="description-image">Paisagem</div>
+                        </div>
+                      </td>
+                      <td class="td-listing">
+                        <div class="content-listing">  
+                          <img class="image-content" src="./fundo.jpg">
+                          <div class="description-image">Paisagem</div>
+                        </div>
+                      </td>
+                      <td class="td-listing">
+                        <div class="content-listing">  
+                          <img class="image-content" src="./fundo.jpg">
+                          <div class="description-image">Paisagem</div>
+                        </div>
+                      </td>
+                      <td class="td-listing">
+                        <div class="content-listing">  
+                          <img class="image-content" src="./fundo.jpg">
+                          <div class="description-image">Paisagem</div>
+                        </div>
+                      </td>
+                      <td class="td-listing">
+                        <div class="content-listing">  
+                          <img class="image-content" src="./fundo.jpg">
+                          <div class="description-image">Paisagem</div>
+                        </div>
+                      </td>
+                      <td class="td-listing">
+                        <div class="content-listing">  
+                          <img class="image-content" src="./fundo.jpg">
+                          <div class="description-image">Paisagem</div>
+                        </div>
+                      </td>
+                      <td class="td-listing">
+                        <div class="content-listing">  
+                          <img class="image-content" src="./fundo.jpg">
+                          <div class="description-image">Paisagem</div>
+                        </div>
+                      </td>
+                      <td class="td-listing">
+                        <div class="content-listing">  
+                          <img class="image-content" src="./fundo.jpg">
+                          <div class="description-image">Paisagem</div>
+                        </div>
+                      </td>
+                      <td class="td-listing">
+                        <div class="content-listing">  
+                          <img class="image-content" src="./fundo.jpg">
+                          <div class="description-image">Paisagem</div>
+                        </div>
+                      </td>
+                      <td class="td-listing">
+                        <div class="content-listing">  
+                          <img class="image-content" src="./fundo.jpg">
+                          <div class="description-image">Paisagem</div>
+                        </div>
+                      </td>
+                      <td class="td-listing">
+                        <div class="content-listing">  
+                          <img class="image-content" src="./fundo.jpg">
+                          <div class="description-image">Paisagem</div>
+                        </div>
+                      </td>
+                      <td class="td-listing">
+                        <div class="content-listing">  
+                          <img class="image-content" src="./fundo.jpg">
+                          <div class="description-image">Paisagem</div>
+                        </div>
+                      </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
