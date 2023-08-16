935fb85c8789f3cc911e8d180a8e33176e74793e
adriel-lab <74466153+adriel-lab@users.noreply.github.com>
2023-08-16 10:45:58 -0300

    SIMILARWEB.js

--- scripts/SIMILARWEB.js
+++ scripts/SIMILARWEB.js
@@ -1,17 +1,15 @@
   <script>
-                function generateAccessLink() {
-                        var linkInput = document.getElementById("link");
-                        var accessLink = document.getElementById("accessLink");
+                        function generateAccessLink() {
+    var linkInput = document.getElementById("link");
+    var accessLink = document.getElementById("accessLink");
 
-                        var linkValue = linkInput.value;
-                        var formattedLink = linkValue.replace(/^https:\/\/www\./i, "");
+    var linkValue = linkInput.value.trim();  // Remove leading/trailing whitespace
+    var formattedLink = linkValue.replace(/^https?:\/\/(?:www\.)?/i, "");
 
-                        var fullAccessLink = "https://www.similarweb.com/website/" + formattedLink;
+    var fullAccessLink = "https://www.similarweb.com/website/" + formattedLink;
 
-
-                        accessLink.innerHTML = "<a href='" + fullAccessLink + "' target='_blank'>Access Link</a>";
-                }
-
+    accessLink.innerHTML = "<a href='" + fullAccessLink + "' target='_blank'>Access Link</a>";
+}
                 document.addEventListener("DOMContentLoaded", function() {
                         var linkInput = document.getElementById("link");
                         linkInput.addEventListener("input", generateAccessLink);
