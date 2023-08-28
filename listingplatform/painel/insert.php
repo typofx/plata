8b85fcf88bb58d2f7060bc632ccbf9e32afc26ea
adriel-lab <74466153+adriel-lab@users.noreply.github.com>
2023-08-28 07:42:24 -0300

    Update insert.php

--- listingplatform/painel/insert.php
+++ listingplatform/painel/insert.php
@@ -1,9 +1,9 @@
 <?php
-session_start(); // Iniciar a sessão
+session_start(); 
 
-// Verificar se o usuário está autenticado
+
 if (!isset($_SESSION["user_logged_in"]) || $_SESSION["user_logged_in"] !== true) {
-    // O usuário não está autenticado, redirecionar de volta para a página de login
+   
     header("Location: index.php");
     exit();
 }
