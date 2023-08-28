a35136a5afca39507fc4b02685ea5506dba0642f
adriel-lab <74466153+adriel-lab@users.noreply.github.com>
2023-08-28 08:15:30 -0300

    Update WHO_IS.js

--- scripts/WHO_IS.js
+++ scripts/WHO_IS.js
@@ -1,17 +1,17 @@
- function generateCountryLink() {
-            var linkInput = document.getElementById("link");
-            var accessLink = document.getElementById("accessCountry");
+function generateCountryLink() {
+        var linkInput = document.getElementById("link");
+        var accessLink = document.getElementById("accessCountry");
 
-            var linkValue = linkInput.value.trim(); // Remove leading/trailing whitespace
-            var formattedLink = linkValue.replace(/^https?:\/\/(?:www\.)?/i, "");
+        var linkValue = linkInput.value.trim(); // Remove leading/trailing whitespace
+        var url = new URL(linkValue);
+        var formattedLink = url.hostname; // Extract the hostname (TLD)
 
-            var fullAccessLink = "https://who.is/whois/" + formattedLink;
+        var fullAccessLink = "https://who.is/whois/" + formattedLink;
 
+        accessLink.innerHTML = "<a href='" + fullAccessLink + "' target='_blank'>WHO.IS</a>";
+    }
 
-            accessLink.innerHTML = "<a href='" + fullAccessLink + "' target='_blank'>WHO.IS</a>";
-        }
-
-        document.addEventListener("DOMContentLoaded", function() {
-            var linkInput = document.getElementById("link");
-            linkInput.addEventListener("input", generateCountryLink);
-        });
+    document.addEventListener("DOMContentLoaded", function() {
+        var linkInput = document.getElementById("link");
+        linkInput.addEventListener("input", generateCountryLink);
+    });
