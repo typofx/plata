7ebadb11568bd83a850197bd8ed4054e1296dc26
adriel-lab <74466153+adriel-lab@users.noreply.github.com>
2024-04-19 10:45:23 -0300

    Update index.php

--- verify/index.php
+++ verify/index.php
@@ -1,78 +1,173 @@
-<?php
-// Function to generate a 6-digit verification code
-function generateVerificationCode() {
-    return mt_rand(100000, 999999);
-}
+<!doctype html>
 
-// Function to send the email with the verification code
-function sendVerificationEmail($email, $code) {
-    $subject = "Verification Code";
-    $message = "Your verification code is: $code";
-    // You can use the PHP mail() function to send the email
-    // Replace the information between <> with your email settings
-    $headers = "From: youremail@yourdomain.com" . "\r\n" .
-               "Reply-To: youremail@yourdomain.com" . "\r\n" .
-               "X-Mailer: PHP/" . phpversion();
+<link rel="stylesheet" href="pix-style.css">
+<script src="https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js"></script>
 
-    // Send the email
-    mail($email, $subject, $message, $headers);
-}
 
-// Check if the form was submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Check if the email field is set
-    if (isset($_POST["email"])) {
-        // Generate a verification code
-        $verification_code = generateVerificationCode();
 
-        // Store the verification code in the session
-        session_start();
-        $_SESSION["verification_code"] = $verification_code;
-        $_SESSION["email"] = $_POST["email"]; // Store the email in the session
-
-        // Send the verification code via email
-        sendVerificationEmail($_POST["email"], $verification_code);
-
-        echo "An email with the verification code has been sent to {$_POST['email']}.";
-    } elseif (isset($_POST["verification_code"])) {
-        // Check if the verification code is correct
-        session_start();
-        if ($_POST["verification_code"] == $_SESSION["verification_code"]) {
-            echo "Verification code correct. The email {$_SESSION['email']} has been successfully verified!";
-            // Here you can add code to mark the email as verified in the database, for example.
-            // Remember to clear the session variables after the verification is completed.
-            unset($_SESSION["verification_code"]);
-            unset($_SESSION["email"]);
-        } else {
-            echo "Incorrect verification code.";
-        }
-    } else {
-        echo "Please provide an email address.";
-    }
-}
-?>
-
-<!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
-    <title>Email Verification</title>
+<title>Plata Token - Gerador de QR Code do PIX</title>
+<meta name="viewport" content="width=device-width, user-scalable=no">
+<meta charset="utf-8">
+<meta name="keywords" content="pix, qrcode pix, qr code, br code, brcode pix, pix copia e cola" />
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
+<script src="https://kit.fontawesome.com/0f8eed42e7.js" crossorigin="anonymous"></script>
+<script>
+function copiar() {
+  var copyText = document.getElementById("brcodepix");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  document.execCommand("copy");
+  document.getElementById("clip_btn").innerHTML='<i class="fas fa-clipboard-check"></i>';
+}
+function reais(v){
+    v=v.replace(/\D/g,"");
+    v=v/100;
+    v=v.toFixed(2);
+    return v;
+}
+function mascara(o,f){
+    v_obj=o;
+    v_fun=f;
+    setTimeout("execmascara()",1);
+}
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value);
+}
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-E6M96X7Y2Y"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-E6M96X7Y2Y');
+</script>
+    <style>
+        .invisibled {
+            font-size: 0px;
+            text-align: center;
+            border-style: none;
+            resize:none;
+            width: 0px;
+            height: 0px;
+        }
+    </style>
 </head>
 <body>
-    <?php if (!isset($_SESSION["verification_code"])): ?>
-    <h2>Email Verification</h2>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email"><br><br>
-        <input type="submit" value="Send Verification Code">
+
+
+<br>
+
+<div id="boxApp" align="center">
+<div id="box" class="box">
+<center><h3>Comprar Plata com Pix</h3></center>
+
+<div id="forml">
+<form method="post" action="0xc298812164bd558268f51cc6e3b8b5daaf0b6341.php" target="_blank">
+
+    <?php
+    date_default_timezone_set('UTC');
+    $date = date("H:i:s T d/m/Y");
+    $Expdate = strtotime(date("H:i:s"))+900;//15*60=900 seconds
+    $Expdate = date("H:i:s T d/m/Y",$Expdate);
+    $_POST["Expdate"] = $Expdate;
+    
+    ?>
+
+    <div class="div-label"> <label for="valor">   Valor a Pagar (BRL)</label></div>
+    <div>
+        <input type="number" id="valorpix" name="valorpix" size="15" autocomplete="off" maxlength="13" step="0.001" min="0.001" value="<?= $_POST["valorpix"];?>" onkeyup="BRLexec()" onkeypress="BRLexec()" onclick="select()" onfocusout="addZeroBRL()" required>
+        <script>
+                
+        </script>
+    </div>
+
+    <div class="div-label"> <label for="PLTwanted">   Tokens Plata Previstos (PLT)</label></div>
+    <div>
+        <input type="number" id="PLTwanted" name="PLTwanted" size="15" step="0.0001" autocomplete="off" maxlength="13" min="0.0001" value="<?= $_POST["PLTwanted"];?>" onkeyup="PLTexec()" onkeypress="PLTexec()" onclick="select()" required>
+    </div>
+
+    <div class="div-label"><label for="email">   Email</label></div>
+    <div>
+        <input type="email" id="emailUser" name="emailUser" size="60" maxlength="90" value="<?= $_POST["emailUser"];?>" onclick="this.select();" required>
+    </div>
+
+    <div class="div-label"><label for="email">  Confirmar Email</label></div>
+    <div>
+        <input type="email" id="confirmemail" name="confirmemail" size="60" maxlength="90" onclick="this.select();" required>
+    </div> 
+    
+    <div>
+        <div class="div-label"><label for="email">   Carteira Web3 Polygon(MATIC)</label></div><div>
+        <input type="text" id="web3wallet" name="web3wallet" placeholder="0x..." size="60" maxlength="42" value="<?= $_POST["web3wallet"];?>" onclick="this.select();" onfocusout="isValidEtherWallet()" required>
+    </div>
+    <div>
+    <div style="display: none;">
+        <label for="identificador">Identificador</label>
+        <input type="text" id="identificador" name="identificador" value="<?php echo (rand(100,999));?>" required>
+    </div>
+    <hr width="95%" class="hrline"/>
+    <button id="submitButton" name="submit" class="buttonBuyNow" onclick="checkemail()">Gerar QR Code <i class="fas fa-qrcode"></i></button>
+    <hr width="95%" class="hrline"/>
     </form>
-    <?php else: ?>
-    <h2>Verify Verification Code</h2>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <label for="verification_code">Verification Code:</label><br>
-        <input type="text" id="verification_code" name="verification_code"><br><br>
-        <input type="submit" value="Verify Code">
-    </form>
-    <?php endif; ?>
+    </div></div></div></div>
+
+<?php include '../en/mobile/price.php';?>
+<br>
+<center><a id="dappVersion">PlataByPix dApp Version 0.1.3 (Beta)</a></center>
+<?php
+    date_default_timezone_set('UTC');
+    echo "<center>" . $Expdate . "</center>";
+?>
+
+<script>
+
+    document.getElementById("valorpix").value = "10.00";
+    let _PLTBRL = <?php echo number_format($PLTBRL, 8, '.' , ''); ?>;
+    console.log("123: "+  _PLTBRL);
+    document.getElementById("txtCurrencyEnv").innerText = "(BRL)";
+    document.getElementById("txtPAIR").innerText = "<?php echo $PLTBRL?>";
+    document.getElementById("tr-price").style.visibility = "collapse";
+    
+    function BRLexec() {
+        let textBRL = Number(document.getElementById("valorpix").value);
+        document.getElementById("PLTwanted").value = Number(textBRL/_PLTBRL).toFixed(4);
+    } BRLexec();
+
+    function addZeroBRL() {
+        document.getElementById("valorpix").value = Number(document.getElementById("valorpix").value).toFixed(2);
+    }
+
+    function PLTexec() {
+        let textPLT = Number(document.getElementById("PLTwanted").value);
+        document.getElementById("valorpix").value = Number(textPLT*_PLTBRL).toFixed(2);
+    }
+    
+    function isValidEtherWallet(){
+        let address = document.getElementById("web3wallet").value;
+        let result = Web3.utils.isAddress(address);
+        if (result!= true) document.getElementById("web3wallet").value = "";
+        console.log(result);  // => true?
+    }  
+    
+    function checkemail(){
+        if ( document.getElementById("emailUser").value != (document.getElementById("confirmemail").value) ) document.getElementById("confirmemail").value = "";
+    }
+    
+</script>
+
+
 </body>
 </html>
-
