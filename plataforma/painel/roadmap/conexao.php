0b5fd0a0828f45ef7d5e4135ebd358c9331f86eb
adriel-lab <74466153+adriel-lab@users.noreply.github.com>
2024-05-06 08:51:29 -0300

    Update conexao.php

--- plataforma/painel/roadmap/conexao.php
+++ plataforma/painel/roadmap/conexao.php
@@ -1,14 +1,14 @@
 <?php
 $servername = "localhost";
-$username = "granna80_user";
-$password = "D72r5vEgt5";
+$username = "";
+$password = "";
 
 
 $conn = new mysqli($servername, $username, $password);
 
 if ($conn->connect_error) {
-    echo "falhou";
-    die("Conexao falhou: " . $conn->connect_error);
+    echo "error";
+    die("error: " . $conn->connect_error);
 }
 ?>
 
