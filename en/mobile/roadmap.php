c449144e03e9f5565e3ad88cc53942854c599e2e
adriel-lab <74466153+adriel-lab@users.noreply.github.com>
2024-05-15 09:10:52 -0300

    Add files via upload

--- en/mobile/roadmap.php
+++ en/mobile/roadmap.php
@@ -10,7 +10,7 @@
     margin-left: auto;
     margin-right: auto;
     height: 47px;
-    }
+}
 
 .roadmap-main-text {
     border-collapse: collapse;
@@ -18,17 +18,17 @@
     height: 80px; /* width="70%" height="80px"*/
     margin-left: auto;
     margin-right: auto;
-    }
+}
     
 .roadmap-main-text .roadmap-header{
     background-color: #4C00B0;
     color: white;
-    }
+}
     
 .roadmap-main-text .roadmap-icon{
     background-color: #8A00C2;
     width: 80px;
-    }
+}
     
 .roadmap-table {
     border-collapse: collapse;
@@ -37,11 +37,10 @@
     margin-top: 30px;
     margin-bottom: 0px;
     text-align: left;
-    }
+}
     
 .roadmap-table h3 {
     text-align: left;
-
 }
 
 .roadmap-table .tdleft {
@@ -59,59 +58,56 @@
     color: white;
 }
 
-.div-roadmap{
+.div-roadmap {
     background-color: #1C0024;
     padding-bottom: 30px;
 }
 
 .highlighted-text {
-        color: yellow;
-        /* You can add more CSS styles as needed */
-    } 
+    color: yellow;
+    /* You can add more CSS styles as needed */
+} 
 </style>
 <style>
-    /* Estilo para ocultar o texto */
-    .texto-oculto {
-      overflow: hidden;
-      position: relative;
+    /* Style to hide text */
+    .hidden-text {
+        overflow: hidden;
+        position: relative;
     }
 
-    /* Estilo para o botão */
-    .botao-mostrar {
-      cursor: pointer;
-      color: blue;
+    /* Style for the button */
+    .show-button {
+        cursor: pointer;
+        color: blue;
     }
 
-    .botao-mostrar {
-    text-align: center; 
-    color: white; 
-    cursor: pointer; 
-   
-    padding: 10px 20px; 
-    border-radius: 5px; 
-  }
+    .show-button {
+        text-align: center; 
+        color: white; 
+        cursor: pointer; 
+        padding: 10px 20px; 
+        border-radius: 5px; 
+    }
 
-  .no-wrap {
-    white-space: nowrap;
-  }
-
-  </style>
+    .no-wrap {
+        white-space: nowrap;
+    }
+</style>
 
 <section id="AnchorRoadmapMobile">
 <div class="div-roadmap">
     
 <table class="roadmap-main-text">
- 	<tr>
-		<td class="roadmap-header"><h2><?php echo  $txtRoadmap ?></h2></td>
-		<td class="roadmap-icon"><img class= "img-line" src="https://www.plata.ie/images/plata-lines-logo.svg" alt=""></td>
-	</tr>
+    <tr>
+        <td class="roadmap-header"><h2><?php echo $txtRoadmap ?></h2></td>
+        <td class="roadmap-icon"><img class="img-line" src="https://www.plata.ie/images/plata-lines-logo.svg" alt=""></td>
+    </tr>
     <td>
 </table>
 
 <?php
 
-
-include 'conexao.php';
+include 'connection.php';
 
 $language = substr($_SERVER['REQUEST_URI'], 1, 2);
 
@@ -140,18 +136,17 @@ if ($resultEN) {
         
         // Check if task_done is equal to 1
         if ($rowEN["task_done"] == 1) {
-          $task_goalEN = '✓ ' . $task_goalEN;
-      } else {
-          $task_goalEN = ' &ndash; ' . $task_goalEN;
-      }
-      
+            $task_goalEN = '✓ ' . $task_goalEN;
+        } else {
+            $task_goalEN = ' &ndash; ' . $task_goalEN;
+        }
 
         // Check if task_highlighted is equal to 1 and add a CSS class
         $task_highlighted = $rowEN["task_highlighted"];
         if ($task_highlighted == 1) {
-            $task_goalEN = '<span class="highlighted-text texto-oculto no-wrap" id="meuTexto" data-texto-completo="' . $task_goalEN . '"><l>' . $task_goalEN . '</l></span>';
+            $task_goalEN = '<span class="highlighted-text hidden-text no-wrap" id="myText" data-full-text="' . $task_goalEN . '"><l>' . $task_goalEN . '</l></span>';
         } else {
-            $task_goalEN = '<span class="texto-oculto no-wrap" id="meuTexto" data-texto-completo="' . $task_goalEN . '"><l>' . $task_goalEN . ' </l></span>';
+            $task_goalEN = '<span class="hidden-text no-wrap" id="myText" data-full-text="' . $task_goalEN . '"><l>' . $task_goalEN . ' </l></span>';
         }
 
         // Check if semester is equal to 1
@@ -167,7 +162,7 @@ if ($resultEN) {
 
     // Populate the Q1 section
     echo '<tr>';
-    echo '<td class="tdleft tdline1"><div class="centre cursor"><h2>S1</h2></div></td>';
+    echo '<td class="tdleft tdline1"><div class="centre cursor"><h2>Q1</h2></div></td>';
     echo '<td class="tdline1 fontStyle"><br>';
     foreach ($q1_tasks as $q1_task) {
         echo $q1_task . '<br>';
@@ -178,7 +173,7 @@ if ($resultEN) {
 
     // Populate the Q2 section
     echo '<tr class="noooo">';
-    echo '<td class="tdleft tdline2 cursor"><div class="centre"><h2>S2</h2></div></td>';
+    echo '<td class="tdleft tdline2 cursor"><div class="centre"><h2>Q2</h2></div></td>';
     echo '<td class="tdline2 fontStyle"><br>';
     foreach ($q2_tasks as $q2_task) {
         echo $q2_task . '<br>';
@@ -190,64 +185,53 @@ if ($resultEN) {
     // Close the HTML table
     echo '</table>';
 } else {
-    echo "Erro na consulta ao banco de dados.";
+    echo "Database query error.";
 }
 ?>
- <!-- Botão para mostrar/ocultar o texto 
- <div class="botao-mostrar" onclick="mostrarOcultarTexto()">Show more</div>-->
+ <!-- Button to show/hide the text 
+ <div class="show-button" onclick="showHideText()">Show more</div>-->
 
   <!--<script>
   document.addEventListener('DOMContentLoaded', function() {
-    mostrarOcultarTexto(true); // Chame a função ao carregar a página
+    showHideText(true); // Call the function when the page loads
   });
 
-  function mostrarOcultarTexto(carregamento) {
-    var spans = document.querySelectorAll('.texto-oculto');
-    var botaoMostrar = document.querySelector('.botao-mostrar');
+  function showHideText(load) {
+    var spans = document.querySelectorAll('.hidden-text');
+    var showButton = document.querySelector('.show-button');
 
     spans.forEach(function(span) {
-      var paragrafo = span.querySelector('l');
-      var palavrasExibidas = 5.5; // Número desejado de palavras
-      var palavras = paragrafo.textContent.split(/\s+/).filter(word => word !== '-' && word.toLowerCase() !== 'de'&& word.toLowerCase() !== 'la');
+      var paragraph = span.querySelector('l');
+      var displayedWords = 5.5; // Desired number of words
+      var words = paragraph.textContent.split(/\s+/).filter(word => word !== '-' && word.toLowerCase() !== 'de' && word.toLowerCase() !== 'la');
 
-     
-
-
-      
-
-      if (carregamento) {
-       
-        var textoResumido = palavras.slice(0, palavrasExibidas).join(' ');
-        paragrafo.innerHTML = textoResumido;
-        botaoMostrar.innerHTML = 'Show more';
+      if (load) {
+        var summaryText = words.slice(0, displayedWords).join(' ');
+        paragraph.innerHTML = summaryText;
+        showButton.innerHTML = 'Show more';
         if (!span.classList.contains('no-wrap')) {
           span.classList.add('no-wrap'); 
         }
       } else {
-     
-        if (palavras.length > palavrasExibidas) {
-          
-          var textoResumido = palavras.slice(0, palavrasExibidas).join(' ');
-          paragrafo.innerHTML = textoResumido;
-          botaoMostrar.innerHTML = 'Show more';
+        if (words.length > displayedWords) {
+          var summaryText = words.slice(0, displayedWords).join(' ');
+          paragraph.innerHTML = summaryText;
+          showButton.innerHTML = 'Show more';
           span.classList.add('no-wrap'); 
         } else {
-        
-          paragrafo.innerHTML = span.getAttribute('data-texto-completo');
-          botaoMostrar.innerHTML = 'Show Less';
+          paragraph.innerHTML = span.getAttribute('data-full-text');
+          showButton.innerHTML = 'Show less';
           span.classList.remove('no-wrap'); 
         }
       }
     });
   }
 
-
-  document.querySelectorAll('.texto-oculto').forEach(function(span) {
-    span.setAttribute('data-texto-completo', span.textContent);
+  document.querySelectorAll('.hidden-text').forEach(function(span) {
+    span.setAttribute('data-full-text', span.textContent);
   });
 </script>
 -->
-
 
 </div>
 </section>
