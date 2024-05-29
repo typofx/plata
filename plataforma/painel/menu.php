dd29736127169f50963b2876a5271bd2fb86a4ab
adriel-lab <74466153+adriel-lab@users.noreply.github.com>
2024-05-29 17:15:19 -0300

    Add files via upload

--- plataforma/painel/menu.php
+++ plataforma/painel/menu.php
@@ -64,6 +64,7 @@ $userName1 = $_SESSION["user_user"];
         <li><a href="team">Meet the Team</a></li>
         <li><a href="email.php">Email</a></li>
         <li><a href="payments">Payments</a></li>
+        <li><a href="giveaway">Giveaway-V2</a></li>
     </ul>
 
    
