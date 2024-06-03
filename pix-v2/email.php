5bf8dd4be1c75229c189b434c055bb7b99c68682
Adriel Dias <74466153+adriel-lab@users.noreply.github.com>
2024-06-03 10:27:21 -0300

    Add files via upload

--- pix-v2/email.php
+++ pix-v2/email.php
@@ -4,32 +4,31 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-// Verifica se a sessão está ativa e se a última atividade foi há menos de 5 minutos
+// Check if the session is active and if the last activity was less than 5 minutes ago
 if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 300)) {
-    // Se a última atividade foi há mais de 5 minutos, destrua a sessão e redirecione para a página inicial
-    session_unset();     // remove todas as variáveis de sessão
-    session_destroy();   // destrói a sessão
-    header("Location: index.php");
+    // If the last activity was more than 5 minutes ago, destroy the session and redirect to the homepage
+    session_unset();     // remove all session variables
+    session_destroy();   // destroy the session
+    header("Location: index");
     exit();
 }
 
-// Atualiza o timestamp da última atividade para o timestamp atual
+// Update the last activity timestamp to the current timestamp
 $_SESSION['LAST_ACTIVITY'] = time();
 
 if (!isset($_SESSION['user'])) {
-    // Se a sessão não estiver iniciada, redireciona de volta para a página inicial
-    header("Location: index.php");
+    // If the session is not started, redirect back to the homepage
+    header("Location: index");
     exit();
 }
 
-
-// Função para gerar um código de verificação aleatório de 6 dígitos
+// Function to generate a random 6-digit verification code
 function generateVerificationCode()
 {
     return mt_rand(100000, 999999);
 }
 
-// Função para enviar o e-mail com o código de verificação
+// Function to send the verification code via email
 function sendVerificationCode($email, $code)
 {
     $subject = 'Plata Token';
@@ -38,7 +37,7 @@ function sendVerificationCode($email, $code)
     return mail($email, $subject, $message, $headers);
 }
 
-// Verifica se o limite de reenvio foi excedido
+// Check if the resend limit has been exceeded
 function checkResendLimit()
 {
     if (!isset($_SESSION['resend_count'])) {
@@ -48,7 +47,7 @@ function checkResendLimit()
     return $_SESSION['resend_count'] < 3;
 }
 
-// Reseta o processo de verificação
+// Reset the verification process
 function resetVerification()
 {
     unset($_SESSION['email']);
@@ -68,64 +67,63 @@ function resetVerification2()
     unset($_SESSION['user']);
 }
 
-// Verifica se o formulário foi submetido
+// Check if the form was submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['send_email']) || isset($_POST['resend_code'])) {
         if (isset($_POST['email']) || isset($_SESSION['email'])) {
             $email = $_POST['email'] ?? $_SESSION['email'];
             if (!isset($_SESSION['code_sent']) || checkResendLimit()) {
-                $_SESSION['email'] = $email; // Armazena o e-mail na sessão
+                $_SESSION['email'] = $email; // Store the email in the session
                 $_SESSION['verification_code'] = generateVerificationCode();
                 $_SESSION['code_sent_time'] = time();
                 sendVerificationCode($email, $_SESSION['verification_code']);
                 $_SESSION['code_sent'] = true;
                 echo $email;
-                
             } else {
                 echo 'Resend limit exceeded. Please, we are resetting the process.';
-                resetVerification2();// Limpa as sessões
-                echo '<script type="text/javascript"> window.location.href = "https://www.plata.ie/pix3/index.php";</script>'; 
+                resetVerification2(); // Clear the sessions
+                echo '<script type="text/javascript"> window.location.href = "https://www.plata.ie/pix3/index";</script>';
             }
         } else {
             echo 'Please enter a valid email address.';
         }
     } elseif (isset($_POST['verify_code'])) {
         if (isset($_POST['verification_code']) && $_POST['verification_code'] == $_SESSION['verification_code']) {
-            echo '<form id="redirectForm" method="post" action="order.php">';
+            echo '<form id="redirectForm" method="post" action="order">';
             echo '<input type="hidden" name="emailUser" value="' . $_SESSION['email'] . '">';
             resetVerification();
             echo '</form>';
             echo '<script>document.getElementById("redirectForm").submit();</script>';
         } else {
             echo 'Incorrect verification code. Please try again.';
-            resetVerification(); // Limpa as sessões
+            resetVerification(); // Clear the sessions
         }
     }
 }
 
-// Verifica se o tempo para confirmação excedeu 5 minutos
+// Check if the time to confirm exceeded 5 minutes
 if (isset($_SESSION['code_sent_time']) && time() - $_SESSION['code_sent_time'] > 300) {
     echo 'Time exceeded to enter the verification code. Please restart the process.';
-    resetVerification(); // Limpa as sessões
+    resetVerification(); // Clear the sessions
 }
 
-// Verifica se há um erro na URL
+// Check if there's an error in the URL
 if (isset($_GET['error'])) {
     switch ($_GET['error']) {
         case 'incorrect_verification_code':
             resetVerification();
-            echo "Código de verificação incorreto. Por favor, tente novamente.";
+            echo "Incorrect verification code. Please try again.";
             break;
         default:
             resetVerification();
-            echo "Ocorreu um erro desconhecido.";
+            echo "An unknown error occurred.";
             break;
     }
 }
 
 if (isset($_GET['cancel'])) {
     resetVerification2();
-    echo '<script type="text/javascript"> window.location.href = "https://www.plata.ie/pix3/index.php";</script>';
+    echo '<script type="text/javascript"> window.location.href = "https://www.plata.ie/pix3/index";</script>';
 }
 if (isset($_GET['order_cancel'])) {
     resetVerification();
@@ -133,20 +131,22 @@ if (isset($_GET['order_cancel'])) {
 }
 
 if (!isset($_SESSION['code_sent'])) {
-    echo '<form id="form1" method="POST">';
-    echo '<label for="email">Email:</label>';
-    echo '<input type="email" name="email" autocomplete="off" required><br><br>';
-    echo '<button type="submit" name="send_email">Send Email</button>';
-    echo '</form>';
+    echo '<form id="form1" method="POST">
+    <label for="email">Email:</label>
+    <input type="email" name="email" autocomplete="off" required><br><br>
+    <button type="submit" name="send_email">Send Email</button>
+    <a href="?cancel=true">Cancel</a>
+  </form>';
 }
-echo '<form id="form2" method="POST">';
-echo '<label for="verification_code">Verification Code:</label>';
-echo '<input type="number" id="verification_code" name="verification_code" max="999999" autocomplete="off"><br>';
-echo '<button type="submit" name="verify_code">Confirmar Email</button>';
-echo '<button type="submit" name="resend_code">Reenviar Código</button>';
-echo '<a href="?cancel=true">Cancelar</a>';
-echo '</form>';
 
-
+if (isset($_SESSION['email'])) {
+    echo '<form id="form2" method="POST">
+    <label for="verification_code">Verification Code:</label>
+    <input type="number" id="verification_code" name="verification_code" max="999999" autocomplete="off"><br>
+    <button type="submit" name="verify_code">Confirm Email</button>
+    <button type="submit" name="resend_code">Resend Code</button>
+    <a href="?cancel=true">Cancel</a>
+  </form>';
+}
 ?>
 <br>
