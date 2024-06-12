869c0d8351a546068fed4ea4852fae7dd7a9fe3f
Adriel Dias <74466153+adriel-lab@users.noreply.github.com>
2024-06-12 09:54:42 -0300

    Add files via upload

--- giveaway-V2/submit_vote.php
+++ giveaway-V2/submit_vote.php
@@ -1,6 +1,6 @@
 <?php
-session_start(); // Iniciar a sessão
-include "conexao.php"; 
+session_start(); // Start the session
+include "connection.php"; 
 ob_start(); include '../en/mobile/price.php'; ob_end_clean();
 
 if ($_SERVER["REQUEST_METHOD"] != "POST") {
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
 }
 
 if (!isset($_POST['evm_wallet']) || !isset($_POST['vote_number'])) {
-    $_SESSION['error'] = "Missing form data.";
+    $_SESSION['error'] = "Form data missing.";
     echo "<script>window.location.href='index.php';</script>";
     exit();
 }
@@ -30,19 +30,23 @@ if (!preg_match('/^0x[a-fA-F0-9]{40}$/', $evm_wallet)) {
     exit();
 }
 
-$check_sql = "SELECT COUNT(*) AS count FROM granna80_bdlinks.votes WHERE evm_wallet = '$evm_wallet'";
-$result = $conn->query($check_sql);
-if ($result && $result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    if ($row["count"] > 0) {
-        $_SESSION['error'] = "This Ethereum wallet address has already been registered.";
+$allowed_duplicate_wallets = array('0xc*****94b550', '0x5******F59B1Fe');
+if (!in_array($evm_wallet, $allowed_duplicate_wallets)) {
+    // Check for duplicates only if it's not one of the allowed addresses
+    $check_sql = "SELECT COUNT(*) AS count FROM granna80_bdlinks.votes WHERE evm_wallet = '$evm_wallet'";
+    $result = $conn->query($check_sql);
+    if ($result && $result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if ($row["count"] > 0) {
+            $_SESSION['error'] = "This Ethereum wallet address has already been registered.";
+            echo "<script>window.location.href='index.php';</script>";
+            exit();
+        }
+    } else {
+        $_SESSION['error'] = "Error checking database for existing wallet address.";
         echo "<script>window.location.href='index.php';</script>";
         exit();
     }
-} else {
-    $_SESSION['error'] = "Error checking database for existing wallet address.";
-   echo "<script>window.location.href='index.php';</script>";
-    exit();
 }
 
 if ($vote_number != 0) {
@@ -52,12 +56,12 @@ if ($vote_number != 0) {
         $vote_row = $vote_result->fetch_assoc();
         if ($vote_row["count"] > 0) {
             $_SESSION['error'] = "A vote with the same number already exists.";
-           echo "<script>window.location.href='index.php';</script>";
+            echo "<script>window.location.href='index.php';</script>";
             exit();
         }
     } else {
         $_SESSION['error'] = "Error checking database for existing vote.";
-       echo "<script>window.location.href='index.php';</script>";
+        echo "<script>window.location.href='index.php';</script>";
         exit();
     }
 }
@@ -72,18 +76,18 @@ if (!isset($_FILES["vote_image"]) || $_FILES["vote_image"]["size"] == 0) {
 } else {
     $check = getimagesize($_FILES["vote_image"]["tmp_name"]);
     if($check === false) {
-        $_SESSION['error'] = "File is not an image.";
-       echo "<script>window.location.href='index.php';</script>";
+        $_SESSION['error'] = "The file is not an image.";
+        echo "<script>window.location.href='index.php';</script>";
         exit();
     }
     if ($_FILES["vote_image"]["size"] > 10000000) { // 10MB
         $_SESSION['error'] = "Sorry, your file is too large.";
-       echo "<script>window.location.href='index.php';</script>";
+        echo "<script>window.location.href='index.php';</script>";
         exit();
     }
     if (!move_uploaded_file($_FILES["vote_image"]["tmp_name"], $target_file)) {
         $_SESSION['error'] = "Sorry, there was an error uploading your file.";
-       echo "<script>window.location.href='index.php';</script>";
+        echo "<script>window.location.href='index.php';</script>";
         exit();
     }
 }
