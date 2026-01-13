8e4373e3a43cf95a352aaa08e16582521ae58e17
Adam Soares <uarloque@live.com>
2026-01-13 23:46:50 +0000

    Create debbuger-cryptoprices.php

---
+++ js/debbuger-cryptoprices.php
@@ -0,0 +1,26 @@
+<?php
+
+$CheckPath_json_wmatic_pool__0x0E1_671a6 = false;
+
+echo"<pre>";
+
+printf ("<b>{: DEBUGGER MODE :}</b> " . gmdate('H:i:s') . " UTC" . "<br>");
+
+if ($ERROR_MSG == null) printf("API OK<br>");
+
+
+
+printf ($PLTMATIC . " PLTMATIC <br>" . $MATICPLT . " MATICPLT <br>" . $USDMATIC . " USDMATIC <br>" . $PLTUSD. " PLTUSD <br>" . $USDPLT. ' USDPLT <br>');
+
+printf ($qtd_plata_pool__0x0E1_671a6 . " PLT / " . $qtd_wmatic_pool__0x0E1_671a6 . " WMATIC" . " (0x0E1...671a6)<br>");
+    
+    if ($CheckPath_json_wmatic_pool__0x0E1_671a6 == true) printf("JSON plt/[ WMATIC ] Pool " . $json_wmatic_pool__0x0E1_671a6.'<br>');
+
+printf ("Circulating Supply : " . $PLTcirculatingSupply . "<br>");
+printf ("Market Cap (USD) : " . $PLTmarketcapUSD . "<br>");
+printf ("Market Cap (BRL) : " . $PLTmarketcapBRL . "<br>");
+printf ("Market Cap (EUR) : " . $PLTmarketcapEUR . "<br>");
+
+echo"</pre>";
+
+?>
